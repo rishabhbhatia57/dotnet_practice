2274ed389a9f23c943e844d05a089cfa5f47a03c
Rishabh <rishabhbhatia57@gmail.com>
2025-12-15 23:43:29 +0530

    NameNTimes

---
+++ NameNTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿//Print name n times using recursion
+//Time Complexity = BigO(N)
+//Space Complexity = BigO(N)
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter number how many times you want to print: ");
+            int n = int.Parse(Console.ReadLine());
+            PrintName(name, n);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+    static int count = 1;
+    public static void PrintName(String name, int n)
+    {
+        if (count > n)
+        {
+            return;
+        }
+        Console.WriteLine(name);
+        count++;
+        PrintName(name, n);
+    }
+}
