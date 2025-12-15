31e01a89bb72cac0b0e8fc9ac95f35c9f046ecc1
Rishabh <rishabhbhatia57@gmail.com>
2025-12-16 00:18:42 +0530

    LineralyOneToN

---
+++ Recursion/LinearlyOneToN/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter last number to print series from 1 to n: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintLinearly(n);
+    }
+    static int count = 1;
+    public static void PrintLinearly(int n)
+    {
+        if (count > n)
+        {
+            return;
+        }
+        Console.WriteLine(count);
+        count++;
+        PrintLinearly(n);
+    }
+}
