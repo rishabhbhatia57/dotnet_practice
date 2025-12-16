1a7aac9f3f578c0ea9ad1f5966a36d43ecd545dd
Rishabh <rishabhbhatia57@gmail.com>
2025-12-16 23:33:43 +0530

    LinearlyNToOne

---
+++ Recursion/LinearlyNtoOne/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value of N to print the series from N to 1: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintLinearly(n);
+    }
+    public static void PrintLinearly(int n)
+    {
+        int count = n;
+        if (count < 1)
+        {
+            return;
+            
+        }
+        Console.WriteLine(count);
+        count--;
+        PrintLinearly(count);
+    }
+}
