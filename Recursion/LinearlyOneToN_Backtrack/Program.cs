3831b632fd33929450e7fd8c49a36a7df8ea013a
Rishabh <rishabhbhatia57@gmail.com>
2025-12-16 23:46:16 +0530

    LinearlyOneToN_Backtrack

---
+++ Recursion/LinearlyOneToN_Backtrack/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter the vaue of N to print the series from 1 to N: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintLinearly(n, n);
+    }
+    public static void PrintLinearly(int i, int n)
+    {
+        if (i < 1) 
+        {
+            return;
+        }
+        PrintLinearly(i-1, n);
+        Console.WriteLine(i);
+    }
+}
