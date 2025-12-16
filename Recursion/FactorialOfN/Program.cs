dc8dd3e7e6635dff60cccd7289284d2764fa0a17
Rishabh <rishabhbhatia57@gmail.com>
2025-12-17 01:04:22 +0530

    Factoril & sum of first N number

---
+++ Recursion/FactorialOfN/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number to pint factorial of n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Factorial of {0} is {1}",n,Factorial(n));
+    }
+
+    public static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+}
---
+++ Recursion/SumOfFirstN/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number to print the sum of n to 1 numbers: ");
+        int n = int.Parse(Console.ReadLine());
+        Sum_Paramterized(n, 0);
+        Console.WriteLine(Sum_Funtional(n));
+    }
+
+    //Sum of first n number using Recursion - Paramterized
+    public static void Sum_Paramterized(int i, int sum)
+    {
+        if (i < 1)
+        {
+            Console.WriteLine("Sum : "+sum);
+            return;
+        }
+        Sum_Paramterized(i-1, sum + i);
+    }
+
+    public static int Sum_Funtional(int n)
+    {
+        int sum = 0;
+        if (n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            sum = n + Sum_Funtional(n - 1);
+        }
+        return sum;
+    }
+
+}
