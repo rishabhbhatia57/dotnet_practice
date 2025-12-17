883c8a5610a1b0e166523518af6072d9fa36a4fe
Rishabh <rishabhbhatia57@gmail.com>
2025-12-18 00:27:58 +0530

    ReverseArray

---
+++ Recursion/ReverseAnArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the size of the Array: ");
+        int size = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the elements of Array: ");
+        int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        ReverseArray(arr, 0, size);
+        Console.WriteLine(string.Join(" ", arr));
+    }
+
+    public static void ReverseArray(int[] arr, int i, int size)
+    {
+        if (i >= size/2)
+        {
+            return;
+        }
+        Swap(ref arr[i], ref arr[size-i-1]);
+        ReverseArray(arr, i+1, size);
+    }
+    static void Swap(ref int a, ref int b)
+    {
+        (a, b) = (b, a);
+    }
+
+}
