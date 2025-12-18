7e651bded80abee10873340397c75e55362f9d94
Rishabh <rishabhbhatia57@gmail.com>
2025-12-18 23:21:56 +0530

    Fibonacci

---
+++ Recursion/Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter nuber to find Fibonacci of number: ");
+        int n = int.Parse(Console.ReadLine());
+        int result = Fibonacci(n);
+        Console.WriteLine(result);
+    }
+    public static int Fibonacci(int n)
+    {
+        if(n <= 1)
+        {
+            return n;
+        }
+        return Fibonacci(n-1) + Fibonacci(n - 2);
+    }
+
+}
