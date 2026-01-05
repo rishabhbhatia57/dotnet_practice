0d8d4766e68f90b81e9cb5a830c0e69025fa5041
Rishabh <rishabhbhatia57@gmail.com>
2026-01-06 01:07:29 +0530

    TwoSum

---
+++ Hashing/TwoSum/Program.cs
@@ -0,0 +1,31 @@
+﻿// Return the indices of elements of Array whose SUM return the target value
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the elements of Array: ");
+        int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        Console.WriteLine("Enter the target value: ");
+        int target = int.Parse(Console.ReadLine());
+        int[] result = TwoSum(arr, target);
+        Console.WriteLine(string.Join(",",result));
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for(int i=0;  i<nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (map.ContainsKey(diff))
+            {
+                return new int[] { map[diff], i };
+            }
+            map[nums[i]] = i;
+        }
+
+        return new int[0];
+    }
+}
